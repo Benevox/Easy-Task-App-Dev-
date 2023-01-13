1d2cd7d1a80d925f591551933ff8df6bf9aefab5
Macaulay Precious <87583799+Precious-Macaulay@users.noreply.github.com>
2023-01-13 21:13:25 +0000

    Sign up and login screen updated

--- easytask-mvp/tailwind.config.js
+++ easytask-mvp/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      "primary-100": "#0956A9",
+      "basic-white": "#FFFFFF",
+      "darkest": "#1F2024",
+      "dark-light": "#71727A",
+      "stroke": "#CACCCF",
+      "soft-dark": "#494D50",
+      "facebook": "#1877F2"
+    },
     extend: {},
   },
   plugins: [],
